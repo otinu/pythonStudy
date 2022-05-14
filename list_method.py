b7c316127f202210c70c2693a589ad81da792b96
otinu <fitshter@gmail.com>
2022-05-14 20:23:02 +0900

    [add]list_method.py

--- list_method.py
+++ list_method.py
@@ -16,3 +16,18 @@ for _ in range(5):
 #カウンターを利用したい場合、カウント用の変数を定義しなくてもカウントができる
 for i, character in enumerate(["a", "b", "c"]):
     print(i, character)
+
+
+# zip関数--------------------------------------------------------------
+#下記コメントアウトにブロック変数を複数回記述させないことで可読性を高められる
+characters1 = ["a", "b", "c"]
+characters2 = ["d", "e", "f"]
+characters3 = ["g", "h", "i"]
+
+"""
+for i in range(len(characrers1)):
+    print(characters1[i], characters2[i], characters3[i])
+"""
+
+for character1, character2, character3 in zip(characters1, characters2, characters3):
+    print(character1, character2, character3)
