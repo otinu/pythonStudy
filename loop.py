2670226f829bc34fc8654d3395bcfb349b0dda13
otinu <fitshter@gmail.com>
2022-05-14 09:57:56 +0900

    [fix]loop.py

--- loop.py
+++ loop.py
@@ -8,3 +8,21 @@ while True:
 
         print(count)
         count += 1
+
+print("========")
+count = 0
+while count < 5:
+    if count == 3:
+        break
+    print(count)
+    count += 1
+else:
+    print('done')
+
+while True:
+    # input()で入力受付
+    word = input('Enter:')
+    num = int(word) # 受け付ける値は全てStringになるため、変換
+    if num == 100:
+        break
+    print('next')
