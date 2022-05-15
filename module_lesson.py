9c373f5ebfe53fd813353422053211cc7ca6c42c
otinu <fitshter@gmail.com>
2022-05-15 16:42:31 +0900

    [fix]module_lesson.py

--- module_lesson.py
+++ module_lesson.py
@@ -10,5 +10,13 @@ print(r)
 """
 #from-importで読み込む方法
 from lesson_package import utils
-r = utils.say_twice("Hello") #該当パッケージ内のファイル名からの記述でOK
+r = utils.say_twice("Hello") #該当パッケージ内のファイル名からの記述
 print(r)
+
+"""
+# "*"でまとめて読み込むには_init_.pyに_all_の追加が必要
+#    ⇒何が使われているのか分かりにくいため、非推奨
+from lesson_package import *
+print(utils.say_twice("Hello")) #該当パッケージ内のファイル名からの記述でOK
+print(fish.swim())
+"""
