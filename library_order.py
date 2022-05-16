b01415f4c3b797222c021ff3fc5f97b9b5492582
otinu <fitshter@gmail.com>
2022-05-16 21:47:21 +0900

    [add]library.py

---
+++ library_order.py
@@ -0,0 +1,15 @@
+#ライブラリのインポートをする順番
+
+# ①標準ライブラリ
+import collections
+import Demos
+import sys
+# ②サードパーティのライブラリ
+
+import termcolor
+# ③自作のパッケージ
+
+import lesson_package
+# ④ローカルファイル
+
+import config
