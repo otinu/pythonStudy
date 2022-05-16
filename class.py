c07345677bb81f6e23f8b91b27a4f316231942e7
otinu <fitshter@gmail.com>
2022-05-16 22:05:10 +0900

    [fix]class.py

--- class.py
+++ class.py
@@ -1,6 +1,17 @@
 class Person(object):
-    def say_something(self):
-        print("hello")
 
-person = Person()
-person.say_something()
+    # __init__でデフォルトコンストラクタを作成
+    ### 引数にselfは必ず必要
+    ### 実質的にnameが第一引数
+    def __init__(self, name):
+
+        ### クラスフィールドのnameに値を入れる
+        self.name = name
+
+    def who_are_you(self):
+        ### 引数なしでも、クラスフィールドのnameに値が入っていれば呼び出せる
+        print("I'm " + self.name)
+
+person = Person("tinu")
+person.who_are_you()
+# person.say_something()
