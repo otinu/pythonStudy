aa74296f5b4c4bffaa9a585e6b7caf66d175fd55
otinu <fitshter@gmail.com>
2022-05-21 10:16:31 +0900

    [fi]DIEc.py

--- DIEx.py
+++ DIEx.py
@@ -1,3 +1,9 @@
+import math #☆①
+from decimal import Decimal #☆②
+import re #☆③
+from struct import pack #☆④
+from struct import unpack
+
 #対話モード時に、最後に表示した式を格納している変数==============
 # ⇒_(アンダバー)
 
@@ -5,9 +11,6 @@
 # ⇒ モジュールが定義している名前を確認することができる。
 
 # math.piは円周率の値をもっている================================
-import math #☆①
-from decimal import Decimal #☆②
-import re
 print(math.pi)
 
 # mathはインポートが必要 ☆①
@@ -19,7 +22,7 @@ test = math.pi * 5
 print("出力結果:")
 print('円周率を5倍すると%2.4fである。'%test)
 
-# Pythonでのdecimal型(固定小数)やfloat型(浮動小数)================
+# decimal型(固定小数)やfloat型(浮動小数)=========================
     #NG
     #print(type(5.1f))
 print(int(10.45))
@@ -30,10 +33,27 @@ print(float(10.45))
 print(Decimal(10.45))
 print(Decimal("10.45"))
 
-# 正規表現
+# 正規表現=======================================================
 # 正規表現はreをインポート ☆③
-
 URL = "http://www.amazon.co.jp/dp/B07T9TCPZX"
 pattern = "https?://[^/]+/"
 res = re.match(pattern, URL)
 print(res.group())
+
+# バイナリデータの取り扱い
+# バイナリデータの取り扱いはstructをインポート ☆④
+
+### "h"は2バイトで表現
+data = pack('hhh', 1, 2, 3)
+print(type(data))
+print(data)
+# ⇒b'\x01\x00\x02\x00\x03\x00'
+
+### "l"は4バイトで表現
+data2 = pack('llh', 1, 2, 3)
+print(type(data2))
+print(data2)
+# ⇒b'\x01\x00\x00\x00\x02\x00\x00\x00\x03\x00'
+
+print(unpack("hhh", data))
+#print(unpack("lll", data)) #アンパックする際はバイト数を合わせる必要がある
