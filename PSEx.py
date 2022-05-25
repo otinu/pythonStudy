df92cc04bcb74142999ac4962d9b587c7e7f52a2
otinu <fitshter@gmail.com>
2022-05-25 21:57:34 +0900

    [add]PSEx.py

--- PSEx.py
+++ PSEx.py
@@ -1,5 +1,6 @@
-# 文字列の置換は何かしらのメソッドを使って行う
-test_str = "HelloWorld"
-print(test_str.replace('Hello', 'Hay'))
-### NG
-#test_str[5] = "G"
+"""
+# ファイルの冒頭で文字コードの指定が可能
+# -*- coding: cp1252 -*-
+
+print("ã�“ã‚“ã�«ã�¡ã‚�")
+"""
